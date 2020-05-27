8033bdd795952b338bb8ceb9699db1bf9a0887f8
Caius Brindescu <caius@etleap.com>
2020-05-27 14:40:26 -0700

    Applied second set of changes

--- lib/fluent/plugin/out_s3.rb
+++ lib/fluent/plugin/out_s3.rb
@@ -539,7 +539,7 @@ module Fluent::Plugin
                                          end
         end
 
-        options[:credentials] = Aws::AssumeRoleCredentials.new(credentials_options)
+        options[:credentials] = EtleapAssumeRoleCredentials.new(credentials_options)
         log.info "Successfully assumed the role #{:role_arn}"
       when @aws_key_id && @aws_sec_key
         options[:access_key_id] = @aws_key_id
@@ -690,6 +690,63 @@ module Fluent::Plugin
 
     def self.register_compressor(name, compressor)
       COMPRESSOR_REGISTRY.register(name, compressor)
+    end
+  end
+
+  ## The following class is an adaptation of: https://github.com/aws/aws-sdk-ruby/blob/master/gems/aws-sdk-core/lib/aws-sdk-core/assume_role_credentials.rb
+  class EtleapAssumeRoleCredentials
+    include Aws::CredentialProvider
+    include Aws::RefreshingCredentials
+
+    # @option options [required, String] :role_arn
+    # @option options [required, String] :role_session_name
+    # @option options [String] :policy
+    # @option options [Integer] :duration_seconds
+    # @option options [String] :external_id
+    # @option options [STS::Client] :client
+    def initialize(options = {})
+      client_opts = {}
+      @assume_role_params = {}
+      options.each_pair do |key, value|
+        if self.class.assume_role_options.include?(key)
+          @assume_role_params[key] = value
+        else
+          client_opts[key] = value
+        end
+      end
+      @client = client_opts[:client] || Aws::STS::Client.new(client_opts)
+      super
+    end
+
+    # @return [STS::Client]
+    attr_reader :client
+
+    private
+
+    def refresh
+      c = @client.assume_role(@assume_role_params).credentials
+      @credentials = Aws::Credentials.new(
+        c.access_key_id,
+        c.secret_access_key,
+        c.session_token
+      )
+      @expiration = c.expiration
+    rescue Aws::STS::Errors::AccessDenied => e
+      # We need to set some credentials, to prevent an NPE further up the call stack.
+      @credentials = Aws::Credentials.new("invalid", "invalid", "invalid")
+      @expiration = 0
+    end
+
+    class << self
+
+      # @api private
+      def assume_role_options
+        @aro ||= begin
+          input = Aws::STS::Client.api.operation(:assume_role).input
+          Set.new(input.shape.member_names)
+        end
+      end
+
     end
   end
 end
